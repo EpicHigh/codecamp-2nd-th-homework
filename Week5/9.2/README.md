32d3255d9a19130588724be5053324526247f8f4
EpicHigh <prasit_tongpradit@windowslive.com>
2018-05-25 12:36:47 +0700

    Initial commit

---
+++ Week5/9.2/README.md
@@ -0,0 +1,3 @@
+# Homework 9.1
+## Promise, Async, Await
+1. From the homework 9.1, Use promise.all() instead
--- Week5/9.2/homework9_2.js
+++ Week5/9.2/homework9_2.js
@@ -7,26 +7,28 @@ contents = ``;
 
 const readHead = async () => {
     let content = await readFile(`head.txt`, `utf8`);
-    contents += `${content}\r\n`;
-    readBody();
+    return `${content}\r\n`;
 };
 
 const readBody = async () => {
     let content = await readFile(`body.txt`, `utf8`);
-    contents += `${content}\r\n`;
-    readLeg();
+    return `${content}\r\n`
 };
 
 const readLeg = async () => {
     let content = await readFile(`leg.txt`, `utf8`);
-    contents += `${content}\r\n`;
-    readFeet();
+    return `${content}\r\n`
 };
 
 const readFeet = async () => {
     let content = await readFile(`feet.txt`, `utf8`);
-    contents += `${content}\r\n`;
-    await writeFile(`robot.txt`, contents, `utf8`);
+    return `${content}\r\n`
 };
 
-readHead();
+Promise.all([readHead(), readBody(), readLeg(), readFeet()])
+    .then(value => {
+        writeFile(`robot.txt`, value, `utf8`);
+    })
+    .catch(error => {
+        console.log(`Error: ${error}`);
+    });
